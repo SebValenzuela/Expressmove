78cf9f8b21351ec0027cb8bb0735aa27f58374ed
Sebastián <seba.val.garat@gmail.com>
2019-08-28 20:24:54 -0400

    Corrección counter

--- test/controllers/counter_controller_test.rb
+++ test/controllers/counter_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class ContadorControllerTest < ActionDispatch::IntegrationTest
+class CounterControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
